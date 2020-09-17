64b4dd3af55830652b3513bd7c7ee036009eb880
Hlynur <hlynurv20@ru.is>
2020-09-17 10:28:02 +0000

    algorithm added

---
+++ tile_traveller.py
@@ -0,0 +1,7 @@
+# •	Markmið forritsins er að notandi komist á endastað (3,1)
+# •	Við viljum spyrja notanda hvaða átt hann vill fara í (N,S,A,V) 
+# •	Ef hann velur vitlaust, þá viljum við spyrja hann aftur
+# •	Ef hann velur rétt þá fær hann gefnar upp nýjar leiðir sem eru í boði
+# •	Þetta er endurtekið þar til hann sigrar (kemst á endastað) þá hættir forritið 
+
+def direction(way)
